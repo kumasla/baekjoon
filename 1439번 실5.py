166ac716ae8b7ab9fcfcea4c2356a4037bd2c085
kumasla <park718513@gmail.com>
2024-04-28 11:59:59 +0900

    최적화 후

--- 1439번 실5.py
+++ 1439번 실5.py
@@ -1,22 +1,24 @@
 s = input()
 
-count = 0
 t_count = 0
 f_count = 0
-change = False
-int 
-i = 1
-x = s[1]
-if(s[1]!= "0") :f_count += 1
-if(s[1]!= "1") : t_count =+1
 
-for i in range(len(s)) :
-    if(s[i] != s[i-1]) :
-        if(count== 0) : 
-            if(s[i] == s[0]) :
-                t_count += 1
-            elif(s[i]) :
-                f_count += 1
+# 첫 번째 문자와 같은지 확인하여 초기값 설정
+if s[0] == "0":
+    t_count += 1
+else:
+    f_count += 1
 
-x = min(t_count,f_count)
+# 두 번째 문자부터 끝까지 반복하여 카운트
+for i in range(1, len(s)):
+    # 현재 문자와 이전 문자가 다르면 카운트 증가
+    if s[i] != s[i-1]:
+        if s[i] == "0":
+            t_count += 1
+        else:
+            f_count += 1
+
+# 두 카운트 중 작은 값을 선택하여 출력
+x = min(t_count, f_count)
 print(x)
+
