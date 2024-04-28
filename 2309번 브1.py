6f12e2f55d6bda82ddebba9628669f797c87e490
kumasla <park718513@gmail.com>
2024-04-28 13:18:03 +0900

    gpt는 신이야 절반이나 줄여줌

--- 2309번 브1.py
+++ 2309번 브1.py
@@ -1,24 +1,12 @@
-a = [0]*9
-for i in range(9) : 
-    a[i] = int(input())
+a = [int(input()) for _ in range(9)]
+a.sort()
+total = sum(a) - 100
 
-a = sorted(a)
-total = sum(a)
-total -= 100
-
-i = 0
-j = 1
-stop = False
-for i in range(8) :
-    for j in range(9) :
-        if(total == a[i] + a[j]) : 
-            stop = True
-            break
-    
-    if(stop == True) : 
-        break
+i,j = 0, 8
+while i < j :
+    if a[i] + a[j] == total : break
+    elif a[i] +a[j] < total : i += 1
+    else : j -= 1
 
 for x in range(9) :
-    if (x != i and x != j) :
-        print(a[x])
-            
+    if x != i and x != j : print(a[x])            
